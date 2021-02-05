7775ece00275c2c98edfca71db8d8e9dcdced51b
Paulo Martins <paulohenrique.sistemas@gmail.com>
2021-02-04 21:58:10 -0300

    Add files via upload

---
+++ TabelaJuros/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TabelaJuros {
+    static class Program {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main() {           
+            Application.Run(new FrmMain());
+        }
+    }
+}
